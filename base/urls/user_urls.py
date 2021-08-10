b73556e13e05915a2fda6b722555a82163c4f228
ssy <seungyun-shin@naver.com>
2021-08-10 17:58:39 +0900

    first commit

---
+++ base/urls/user_urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from base.views import user_views as views
+
+urlpatterns = [
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('', views.getRoutes, name="routes"),
+
+    path('register/', views.registerUser, name='register'),
+
+    path('profile/', views.getUserProfile, name="users-profile"),
+    path('profile/update/', views.updateUserProfile, name="users-profile-update"),
+    path('', views.getUsers, name="users"),
+]
